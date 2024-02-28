39d095bb8c12046c5814d1d57b077c9ad01bd424
IsraelRAlencar <israelalencar@gecontrolsistemas.com.br>
2024-02-28 10:59:23 -0400

    new - adicionando navegacao para uma nova tela e estilos

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,26 @@ import 'screens/categories_screen.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  MyApp({super.key});
+  final ThemeData theme = ThemeData(
+    fontFamily: 'Raleway',
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DeliMeals',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: theme.copyWith(
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Colors.pink,
+            secondary: Colors.amber,
+            background: const Color.fromRGBO(255, 254, 229, 1),
+          ),
+          textTheme: const TextTheme().copyWith(
+              titleLarge: const TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotoCondensed',
+          ))),
       home: const CategoriesScreen(),
     );
   }
